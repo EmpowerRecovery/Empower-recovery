8f75f905f71df931a055bd94f8d721bea76defe3
Hawleywoo <alhawley21@gmail.com>
2023-01-16 22:20:24 -0700

    UPdated styling and responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from './Header'
 import ClimbingImg from '../assets/images/SunsetClimbing.jpeg'
 
 export default function Hero() {
   return (
     <div className="hero">
+      <Header />
       <img className="hero__image" src={ClimbingImg} alt="Perservering the climb of a mountain" />
+      <h4>One of the Denver Metro's premiere tranistional sober living facilities.</h4>
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,9 @@
 import React, { Suspense, lazy } from 'react'
 import Hiking from '../assets/images/FourGuysHiking.jpg';
 import LogoNoText from '../assets/images/logo-no-background-no-text.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-regular-svg-icons'
+
 
 export default function Main() {
   return (
@@ -10,24 +13,30 @@ export default function Main() {
           <img src={LogoNoText} alt="Empower Recovery logo" /> 
           <h1>Empower Recovery</h1>
         </div>
-        <h4>One of the Denver Metro's premiere tranistional sober living facilities.</h4>
+        <h4>Empowering you to thrive in your continued soberity.</h4>
       </div>
       <div className="main__description-container">
         <img id="main-hiking" src={Hiking} alt="Four guys hiking empowering each other to finish the hike."/>
         <div className="main__description-container__description">
-            <h2>Empowering Each Others Soberity</h2>
+            <h3>Empowering Each Others Soberity</h3>
             <p>Join us on your journey to recovery. We offer a community of support to men in transitional living through structure, accountability, and camaraderie. Allow us to assist you in your journey to a new way of thinking and living. </p>
+            {/* <a id="call-us" href="tel:+17205309809">
+              <FontAwesomeIcon icon="fa-regular fa-phone" />
+              <h4>(720)530-9809</h4>
+            </a> */}
         </div>
       </div>
+      <div className="main__benefits-container">
       <div className="main__benefits">
-        <h2>Benefits of Transitional Sober Living</h2>
+        <h3>Benefits of Transitional Sober Living</h3>
         <ul className="main__benefits--list">
-          <li>Provides structure which can be helpful during the transition between treatment and independent living</li>
-          <li>Community of like minded peers provide support and feedback</li>
-          <li>Accountability of residents by requiring to stay substance free</li>
-          <li>People who live in a sober environment after treatment are less likely to relapse than people who don’t</li>
-          <li>Maintain momentum after discharging from rehab</li>
+          <li><span>Provides structure which can be helpful during the transition between treatment and independent living</span></li>
+          <li><span>Community of like minded peers provide support and feedback</span></li>
+          <li><span>Accountability of residents by requiring to stay substance free</span></li>
+          <li><span>People who live in a sober environment after treatment are less likely to relapse than people who don’t</span></li>
+          <li><span>Maintain momentum after discharging from rehab</span></li>
         </ul>
+      </div>
       </div>
     </section>
   )
