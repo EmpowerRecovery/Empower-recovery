125d1d04f88cb78b7b3c5a5265016f15c831f96f
Hawleywoo <alhawley21@gmail.com>
2023-01-17 22:22:32 -0700

    Styling changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,28 +2,32 @@ import React, { Suspense, lazy } from 'react'
 import Hiking from '../assets/images/FourGuysHiking.jpg';
 import LogoNoText from '../assets/images/logo-no-background-no-text.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-regular-svg-icons'
-
+// import { faPhone } from '@fortawesome/free-regular-svg-icons'
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function Main() {
   return (
     <section className="main">
       <div className="main__header">
-        <div className="main__header__title">
+        {/* <div className="main__header__title">
           <img src={LogoNoText} alt="Empower Recovery logo" /> 
           <h1>Empower Recovery</h1>
         </div>
-        <h4>Empowering you to thrive in your continued soberity.</h4>
+        <h4>Empowering you to thrive in your continued oberity.</h4> */}
       </div>
       <div className="main__description-container">
         <img id="main-hiking" src={Hiking} alt="Four guys hiking empowering each other to finish the hike."/>
         <div className="main__description-container__description">
-            <h3>Empowering Each Others Soberity</h3>
+        <div className="main__header__title">
+          <img src={LogoNoText} alt="Empower Recovery logo" /> 
+          <h1>Empower Recovery</h1>
+        </div>
+          <h3>Empowering Each Others Soberity</h3>
             <p>Join us on your journey to recovery. We offer a community of support to men in transitional living through structure, accountability, and camaraderie. Allow us to assist you in your journey to a new way of thinking and living. </p>
-            {/* <a id="call-us" href="tel:+17205309809">
-              <FontAwesomeIcon icon="fa-regular fa-phone" />
-              <h4>(720)530-9809</h4>
-            </a> */}
+              <a id="call-us" className="button" href="tel:+17205309809">
+                <FontAwesomeIcon id="phone" icon={faPhone} />
+                <h4>720-530-9809</h4>
+              </a>
         </div>
       </div>
       <div className="main__benefits-container">
